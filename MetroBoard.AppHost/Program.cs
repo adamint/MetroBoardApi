948b1869e9d501156140fef3ef4edbaa5a4b4fd8
Ratzman, Adam Mortimer <aratzman@iu.edu>
2025-02-16 23:55:51 -0500

    add environment variables, update endpoints

--- MetroBoard.AppHost/Program.cs
+++ MetroBoard.AppHost/Program.cs
@@ -1,6 +1,8 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
 builder.AddProject<Projects.MetroBoard_Api>("metro-api")
+    .WithEnvironment("WMATA_API_KEY", Environment.GetEnvironmentVariable("WMATA_API_KEY"))
+    .WithEnvironment("SLEEP_TIME", Environment.GetEnvironmentVariable("SLEEP_TIME"))
     .WithExternalHttpEndpoints();
 
 builder.Build().Run();
